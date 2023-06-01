ddc4c3dcc170c32e51c66c246108ef3523efc60c
Dhiovana <dhiovana.moreira@aluno.cefet-rj.br>
2023-06-01 14:56:45 -0300

    Add subtração e adição

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 using namespace std;
 
+int calculo(int num1, int num2, int calcOperador){
+	int resultado;
+	switch(calcOperador){
+	case '+';
+	result = num1 + num2;
+	break;
+	case '-';
+	result = num1 - num2;
+	break;
+	}
+	return resultado;
+}
+
 int main{
-	int num1, num2, ;
-	char operador;
+	int num1, num2, aux, resultCalc;
+	char calcOperador;
 	cout << "Escolha o primeiro número \n";
 	cin >> num1;
 	cout << "Escolha o segundo número \n";
 	cin >> num2;
 	cout << "Escolha a operação necessária: ('+' para somar, '-' para subtrair, '*' para multiplicar e '/' para dividir) \n";
-	cin >> operador;
+	cin >> calcOperador;
 }
