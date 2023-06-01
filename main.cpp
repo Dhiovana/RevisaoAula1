6c7f5a7d31a524c969f85273bf97e1134d5b95dc
Dhiovana <dhiovana.moreira@aluno.cefet-rj.br>
2023-06-01 15:16:27 -0300

    Calculadora finalizada

--- main.cpp
+++ main.cpp
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 using namespace std;
 
-int calculo(int num1, int num2, int calcOperador){
+int calculo(int num1, int num2, int calcOperador, int &aux){
 	int resultado;
 	switch(calcOperador){
-	case '+';
-	result = num1 + num2;
+	case '+':
+	resultado = num1 + num2;
 	break;
-	case '-';
-	result = num1 - num2;
+	case '-':
+	resultado = num1 - num2;
 	break;
-	case '*';
-	result = num1 * num2;
+	case '*':
+	resultado = num1 * num2;
 	break;
-	case '/';
-	result = num1 / num2;
+	case '/':
+	resultado = num1 / num2;
 	break;
 	default:
-	aux = 0
+	aux = 0;
 	return 0;
     }
     aux = 1;
 	return resultado;
 }
 
-int main{
-	int num1, num2, aux, resultCalc;
+int main(){
+	int num1, num2, aux, resultadoCalc;
 	char calcOperador;
 	cout << "Escolha o primeiro número \n";
 	cin >> num1;
@@ -34,4 +34,6 @@ int main{
 	cin >> num2;
 	cout << "Escolha a operação necessária: ('+' para somar, '-' para subtrair, '*' para multiplicar e '/' para dividir) \n";
 	cin >> calcOperador;
+	resultadoCalc = calculo(num1, num2, calcOperador, aux);
+	aux != 1 ? cout<< "Operador inválido, tente novamente" : cout<< num1<<calcOperador<<num2<<"="<<resultadoCalc;
 }
