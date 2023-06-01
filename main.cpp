00ea63f20c038af3867c03a9254e7d2bdab5c7bc
Dhiovana <dhiovana.moreira@aluno.cefet-rj.br>
2023-06-01 15:00:31 -0300

    Add multiplicação e divisão

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ int calculo(int num1, int num2, int calcOperador){
 	case '-';
 	result = num1 - num2;
 	break;
+	case '*';
+	result = num1 * num2;
+	break;
+	case '/';
+	result = num1 / num2;
+	break;
 	}
 	return resultado;
 }
